d420f2232e16c2e8879ef0e668a3a8b2f0047b3e
Athul Dev <36979552+athul-dev@users.noreply.github.com>
2018-03-12 21:28:04 +0530

    Decision Tree model from scratch without library
    Decision Tree model from scratch without using the library functions, and by making use of the Gini index and the Information Gain.

---
+++ decision-tree-math.py
@@ -0,0 +1,205 @@
+from __future__ import print_function
+
+training_data = [
+    ['Sunny','Hot','High','Weak','No'],
+    ['Sunny','Hot','High','Strong','No'],
+    ['Overcast','Hot','High','Weak','Yes'],
+    ['Rain', 'Mild', 'High', 'Weak', 'Yes'],
+    ['Rain','Cool','Normal','Weak','Yes'],
+    ['Rain','Cool','Normal','Strong','No'],
+    ['Overcast','Cool','Normal','Strong','Yes'],
+    ['Sunny','Mild','High','Weak','No'],
+    ['Sunny','Cool','Normal','Weak','Yes'],
+    ['Rain','Mild','Normal','Weak','Yes'],
+    ['Sunny','Mild','Normal','Strong','Yes'],
+    ['Overcast','Mild','High','Strong','Yes'],
+    ['Overcast','Hot','Normal','Weak','Yes'],
+    ['Rain','Mild','High','Strong','No'],
+]
+
+header=["Outlook","Temperature","Humidity","Wind","PlayTennis"]
+
+def unique_vals(rows, col):
+    return set([row[col] for row in rows]) #Find the unique values for a column in a dataset.
+#print(unique_vals(training_data,0))
+
+def class_counts(rows):
+    #"""Counts the number of each type of example in a dataset."""
+    counts = {}  # a dictionary of label -> count.
+    for row in rows:
+        # in our dataset format, the label is always the last column
+        label = row[-1]
+        if label not in counts:
+            counts[label] = 0
+        counts[label] += 1
+    return counts
+
+#print(class_counts(training_data))
+
+def is_numeric(value):
+    return isinstance(value, int) or isinstance(value, float)
+
+#print(is_numeric("Red"))
+
+class Question:
+    def __init__(self, column, value):
+        self.column = column
+        self.value = value
+
+    def match(self, example):
+        # Compare the feature value in an example to the
+        # feature value in this question.
+        val = example[self.column]
+        if is_numeric(val):
+            return val >= self.value
+        else:
+            return val == self.value
+
+    def __repr__(self):
+        # This is just a helper method to print
+        # the question in a readable format.
+        condition = "=="
+        if is_numeric(self.value):
+            condition = ">="
+        return "Is %s %s %s?" % (header[self.column], condition, str(self.value))
+
+
+def partition(rows, question):
+    true_rows, false_rows = [], []
+    for row in rows:
+        if question.match(row):
+            true_rows.append(row)
+        else:
+            false_rows.append(row)
+    return true_rows, false_rows
+
+def gini(rows):
+    counts = class_counts(rows)
+    impurity = 1
+    for label in counts:
+        prob_of_label = counts[label] / float(len(rows))
+        impurity -= prob_of_label**2
+    return impurity
+
+def info_gain(left, right, current_uncertainty):
+    # """Information Gain.
+    # The uncertainty of the starting node, minus the weighted impurity of
+    # two child nodes.
+    # """
+    p = float(len(left)) / (len(left) + len(right))
+    return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
+
+def find_best_split(rows):
+    # """Find the best question to ask by iterating over every feature / value
+    # and calculating the information gain."""
+    best_gain = 0  # keep track of the best information gain
+    best_question = None  # keep train of the feature / value that produced it
+    current_uncertainty = gini(rows)
+    n_features = len(rows[0]) - 1  # number of columns
+
+    for col in range(n_features):  # for each feature
+
+        values = set([row[col] for row in rows])  # unique values in the column
+
+        for val in values:  # for each value
+
+            question = Question(col, val)
+
+            # try splitting the dataset
+            true_rows, false_rows = partition(rows, question)
+
+            # Skip this split if it doesn't divide the
+            # dataset.
+            if len(true_rows) == 0 or len(false_rows) == 0:
+                continue
+
+            # Calculate the information gain from this split
+            gain = info_gain(true_rows, false_rows, current_uncertainty)
+
+            # You actually can use '>' instead of '>=' here
+            # but I wanted the tree to look a certain way for our
+            # toy dataset.
+            if gain >= best_gain:
+                best_gain, best_question = gain, question
+
+    return best_gain, best_question
+
+class Leaf:
+    # """A Leaf node classifies data.
+    # This holds a dictionary of class (e.g., "Apple") -> number of times
+    # it appears in the rows from the training data that reach this leaf.
+    # """
+
+    def __init__(self, rows):
+        self.predictions = class_counts(rows)
+
+class Decision_Node:
+    # """A Decision Node asks a question.
+    # This holds a reference to the question, and to the two child nodes.
+    # """
+
+    def __init__(self,
+                 question,
+                 true_branch,
+                 false_branch):
+        self.question = question
+        self.true_branch = true_branch
+        self.false_branch = false_branch
+
+def build_tree(rows):
+    """Builds the tree.
+    Rules of recursion: 1) Believe that it works. 2) Start by checking
+    for the base case (no further information gain). 3) Prepare for
+    giant stack traces.
+    """
+
+    # Try partitioing the dataset on each of the unique attribute,
+    # calculate the information gain,
+    # and return the question that produces the highest gain.
+    gain, question = find_best_split(rows)
+
+    # Base case: no further info gain
+    # Since we can ask no further questions,
+    # we'll return a leaf.
+    if gain == 0:
+        return Leaf(rows)
+
+    # If we reach here, we have found a useful feature / value
+    # to partition on.
+    true_rows, false_rows = partition(rows, question)
+
+    # Recursively build the true branch.
+    true_branch = build_tree(true_rows)
+
+    # Recursively build the false branch.
+    false_branch = build_tree(false_rows)
+
+    # Return a Question node.
+    # This records the best feature / value to ask at this point,
+    # as well as the branches to follow
+    # dependingo on the answer.
+    return Decision_Node(question, true_branch, false_branch)
+
+def print_tree(node, spacing=""):
+    """World's most elegant tree printing function."""
+
+    # Base case: we've reached a leaf
+    if isinstance(node, Leaf):
+        print (spacing + "Predict", node.predictions)
+        return
+
+    # Print the question at this node
+    print (spacing + str(node.question))
+
+    # Call this function recursively on the true branch
+    print (spacing + '--> True:')
+    print_tree(node.true_branch, spacing + "  ")
+
+    # Call this function recursively on the false branch
+    print (spacing + '--> False:')
+    print_tree(node.false_branch, spacing + "  ")
+
+mytree=build_tree(training_data)
+print_tree(mytree)
+
+
